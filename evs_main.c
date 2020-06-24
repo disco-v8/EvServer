28a21f1b3be737e554d27c26d55942664cec2c78
T.Kabu <kabu@fvg-on.net>
2020-06-24 16:13:35 +0900

    Update evs_main.c

--- evs_main.c
+++ evs_main.c
@@ -15,7 +15,7 @@
 //     0.0.9  Change to Non-Blocking FD
 //     0.0.10 No-communication timer
 //     0.0.11 Bug fix free() and ?alloc() parameter.
-//     0.x.x  Daemon mode support. (TBD)
+//     0.0.12 Daemon mode support.
 //     0.x.x  Added API for upper module. (TBD)
 //     0.x.x  Added PostgreSQL module. (TBD)
 //     0.x.x  Added PostgreSQL module. (TBD)
@@ -54,34 +54,75 @@
 // ----------------------------------------------------------------------
 // コード部分
 // ----------------------------------------------------------------------
+// --------------------------------
+// ログ出力処理
+// --------------------------------
+void logging(int level, char * logstr)
+{
+    // デーモンモードではないなら
+    if (EVS_config.daemon != 1)
+    {
+        // 標準出力
+        printf("%s : %s", loglevel_list[level], logstr);
+    }
+    else
+    {
+        // ログファイル名の指定があるのにログファイルが開いていなければ
+        if (EVS_config.log_file != NULL && EVS_log_fd == 0)
+        {
+            // ログファイルを書き込みで開く
+            EVS_log_fd = open(EVS_config.log_file, (O_WRONLY | O_CREAT | O_APPEND), (S_IRUSR | S_IWUSR | S_IRGRP | S_IROTH));
+        }
+        // ログファイルが開けていないなら
+        if (EVS_log_fd == 0 || EVS_log_fd == -1)
+        {
+            // 標準出力
+            printf("%s : %s", loglevel_list[level], logstr);
+        }
+        else
+        {
+            // ログファイルに出力
+            write(EVS_log_fd, logstr, strlen(logstr));
+        }
+    }
+}
 
 // --------------------------------
 // メイン処理
 // --------------------------------
 int main (int argc, char *argv[])
 {
+    int                             result;
+    char                            log_str[MAX_LOG_LENGTH];
+
     // --------------------------------
     // 初期化処理
     // --------------------------------
-    INIT_all(argc, argv);
+    result = INIT_all(argc, argv);
 
-    // ----------------
-    // イベントループ開始(libev Ver4.x以降はev_run() 他にもいくつか変更点あり。flag=0がデフォルトで、ノンブロッキングはEVRUN_NOWAIT=1、一度きりはEVRUN_ONCE=2)
-    // ----------------
-    printf("INFO  : ev_run(): Go!\n");                                  // en_run()から出てくるときはイベントループが終わった時なので、呼ぶ前にログを出力
-    ev_run(EVS_loop, 0);
-
+    // 処理結果がOK(=0)なら
+    if (result == 0)
+    {
+        // ----------------
+        // イベントループ開始(libev Ver4.x以降はev_run() 他にもいくつか変更点あり。flag=0がデフォルトで、ノンブロッキングはEVRUN_NOWAIT=1、一度きりはEVRUN_ONCE=2)
+        // ----------------
+        snprintf(log_str, MAX_LOG_LENGTH, "%s(): ev_run(): Go!\n", __func__);               // daemon(0, 0): を呼ぶ前にログを出力
+        logging(LOGLEVEL_INFO, log_str);
+        ev_run(EVS_loop, 0);
+    }
     // ----------------
     // 作成したイベントループの破棄
     // ----------------
     ev_loop_destroy(EVS_loop);
-    printf("INFO  : ev_loop_destroy(): OK.\n");
+    snprintf(log_str, MAX_LOG_LENGTH, "%s(): ev_loop_destroy(): Go!\n", __func__);          // daemon(0, 0): を呼ぶ前にログを出力
+    logging(LOGLEVEL_INFO, log_str);
 
     // --------------------------------
     // 終了処理
     // --------------------------------
     CLOSE_all();
-    printf("INFO  : CLOSE_all(): OK.\n");
+    snprintf(log_str, MAX_LOG_LENGTH, "%s(): CLOSE_all(): Go!\n", __func__);                // daemon(0, 0): を呼ぶ前にログを出力
+    logging(LOGLEVEL_INFO, log_str);
 
     return 0;
 }
